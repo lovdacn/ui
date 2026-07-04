a22aa46a7c81991c176ad9793dea2dfa8087ea8e
manideep1428 <saimanideep.ch12345@gmail.com>
2026-07-04 15:30:00 +0000

    feat(cli): add core index entrypoint and commander CLI configuration

---
+++ packages/lovda/src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { Command } from "commander"
+import { init } from "./commands/init"
+import { add } from "./commands/add"
+
+const program = new Command()
+  .name("lovda")
+  .description("A CLI to initialize and manage Expo/NativeWind projects")
+  .version("0.1.0")
+
+program.addCommand(init)
+program.addCommand(add)
+
+program.parse(process.argv)
