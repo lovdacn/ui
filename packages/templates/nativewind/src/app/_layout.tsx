e84eadfb61cd9f35848f17c84532f3fdd6547952
manideep1428 <saimanideep.ch12345@gmail.com>
2026-07-11 20:11:59 +0530

    fix: wrap template layouts in GestureHandlerRootView for native overlay open

--- packages/templates/nativewind/src/app/_layout.tsx
+++ packages/templates/nativewind/src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { PortalHost } from '@rn-primitives/portal';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
@@ -12,9 +14,12 @@ SplashScreen.preventAutoHideAsync();
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <AnimatedSplashOverlay />
+        <AppTabs />
+        <PortalHost />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
